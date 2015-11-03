8050f258f502342a5e574e63ad519d52d0069d5e
Mimi Schatz <mcschatz@gmail.com>
2015-11-03 12:56:15 -0700

    invoice_items controller index and show test

---
+++ test/controllers/api/v1/invoice_items_controller_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class Api::V1::InvoiceItemsControllerTest < ActionController::TestCase
+  test "#index" do
+    get :index, format: :json
+
+    assert_response :success
+  end
+
+  test "#index returns the right number of objects" do
+    number_of_invoice_items = InvoiceItem.count
+    get :index, format: :json
+
+    json_response = JSON.parse(response.body)
+
+    assert_equal number_of_invoice_items, json_response.count
+  end
+
+  test "#show" do
+    invoice_item = invoice_items(:one)
+    get :show, id: invoice_item.id, format: :json
+
+    assert_response :success
+  end
+
+  test "#show returns the right object name" do
+    invoice_item = invoice_items(:one)
+    get :show, id: invoice_item.id, format: :json
+
+    json_response = JSON.parse(response.body)
+    assert_equal json_response["quantity"], "10"
+  end
+end
