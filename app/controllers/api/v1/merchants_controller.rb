f94f8b3f9407d8cfb9f97ac2f9cd1a17c190e40d
Mimi Schatz <mcschatz@gmail.com>
2015-11-02 23:04:14 -0700

    index and show for each csv file

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,37 +1,11 @@
 class Api::V1::MerchantsController < ApplicationController
-  respond_to :json, :xml
-
-  # before_action :authenticate!
+  respond_to :json
 
   def index
     respond_with Merchant.all
   end
 
-  # def show
-  #   respond_with Merchant.find(params[:id])
-  # end
-
-  # def create
-  #   respond_with Merchant.create(item_params)
-  # end
-
-  # def update
-  #   respond_with Merchant.update(params[:id], item_params)
-  # end
-
-  # def destroy
-  #   respond_with Merchant.destroy(params[:id])
-  # end
-
-  # private
-
-  # def item_params
-  #   params.require(:item).permit(:name, :description, :image_url)
-  # end
-
-  # def authenticate!
-  #   authenticate_or_request_with_http_token do |token|
-  #     ApiToken.exists?(token: token)
-  #   end
-  # end
+  def show
+    respond_with Merchant.find(params[:id])
+  end
 end
