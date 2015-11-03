1b6c03ae4acca0b4dfa355ba6e9016d83d88f980
Mimi Schatz <mcschatz@gmail.com>
2015-11-03 13:35:40 -0700

    items controller show and index tests

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -8,4 +8,17 @@ class Api::V1::InvoiceItemsController < ApplicationController
   def show
     respond_with InvoiceItem.find(params[:id])
   end
+  def find
+    respond_with InvoiceItem.find_by(invoice_item_params)
+  end
+
+  def find_all
+    respond_with InvoiceItem.where(invoice_item_params)
+  end
+
+  private
+
+  def invoice_item_params
+    params.permit(:id, :quantity, :unit_price, :invoice_id, :item_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,47 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :merchants, only: [:index,:show]
-      resources :invoices, only: [:index,:show]
-      resources :items, only: [:index,:show]
-      resources :transactions, only: [:index,:show]
-      resources :customers, only: [:index,:show]
-      resources :invoice_items, only: [:index,:show]
+      resources :merchants, only: [:index,:show] do
+        collection do
+          get 'find'
+          get 'find_all'
+        end
+      end
+
+      resources :invoices, only: [:index,:show] do
+        collection do
+          get 'find'
+          get 'find_all'
+        end
+      end
+
+      resources :items, only: [:index,:show] do
+        collection do
+          get 'find'
+          get 'find_all'
+        end
+      end
+
+      resources :transactions, only: [:index,:show] do
+        collection do
+          get 'find'
+          get 'find_all'
+        end
+      end
+
+      resources :customers, only: [:index,:show] do
+        collection do
+          get 'find'
+          get 'find_all'
+        end
+      end
+
+      resources :invoice_items, only: [:index,:show] do
+        collection do
+          get 'find'
+          get 'find_all'
+        end
+      end
     end
   end
 end
