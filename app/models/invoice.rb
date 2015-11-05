db8185148f8d2ace759b84fc44a2463b35119bdc
Mimi Schatz <mcschatz@gmail.com>
2015-11-05 10:48:29 -0700

    removed unused line of code

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,7 +5,6 @@ class Invoice < ActiveRecord::Base
   belongs_to :merchant
   belongs_to :customer
 
-  scope :by_date, -> (date) { where(created_at: date)}
   scope :successful, -> { joins(:transactions).merge(Transaction.successful) }
 
   def self.pending
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,5 @@ class Transaction < ActiveRecord::Base
   belongs_to :invoice
 
   scope :successful, -> { where(result: "success") }
-
   scope :unsuccessful, -> { distinct.where(result: "failed")}
 end
