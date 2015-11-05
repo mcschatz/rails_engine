b5a09d65a54c4bf0d77f9a4501b7e3dbdb27ac33
Mimi Schatz <mcschatz@gmail.com>
2015-11-05 00:41:04 -0700

    item controller test completed

--- test/controllers/api/v1/invoice_items_controller_test.rb
+++ test/controllers/api/v1/invoice_items_controller_test.rb
@@ -30,4 +30,53 @@ class Api::V1::InvoiceItemsControllerTest < ActionController::TestCase
     json_response = JSON.parse(response.body)
     assert_equal json_response["quantity"], 10
   end
+
+  test "#find" do
+    get :find, format: :json, quantity: "2"
+    invoice_item = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal 2, invoice_item[:quantity]
+    assert_equal "25.0", invoice_item[:unit_price]
+    assert_equal 980190962, invoice_item[:item_id]
+    assert_equal 113629430, invoice_item[:invoice_id]
+  end
+
+  test "#find_all" do
+    get :find_all, format: :json, unit_price: "25.0"
+    invoice_items = JSON.parse(response.body, symbolize_names: true)
+    invoice_item = invoice_items.first
+
+    assert_response :success
+    assert_equal 2, invoice_item[:quantity]
+    assert_equal "25.0", invoice_item[:unit_price]
+    assert_equal 980190962, invoice_item[:item_id]
+    assert_equal 113629430, invoice_item[:invoice_id]
+  end
+
+  test "#random" do
+    get :random, format: :json
+    invoice_item = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal 1, invoice_item.count
+  end
+
+  test "#invoice" do
+
+    get :invoice, format: :json, id: InvoiceItem.last.id
+    invoice = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal 113629430, invoice[:customer_id]
+  end
+
+  test "#item" do
+
+    get :item, format: :json, id: InvoiceItem.first.id
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal "Coffee", item[:name]
+  end
 end
